8f211f9bac15db165793c9d5e0aecc642300a6d8
VANNY.CHAN <VANNY.CHAN@PNC.LAN>
2024-02-23 10:20:02 +0700

    ['fix']edit category EL-65

--- controllers/admin/category/admin_edit_categories.controller.php
+++ controllers/admin/category/admin_edit_categories.controller.php
@@ -3,9 +3,13 @@ require "../../../database/database.php";
 require "../../../models/admin.model.php";
 
 
-$id = $_GET['id'] ? $_GET['id'] : null;
-if (isset($id))
-{
-   $categories = getCategory($id);
-    header('Location: /add_category');
+$id = isset($_GET['id'])  ? $_GET['id'] : null;{
+//    $categories = getCategory($id);
+    $name = htmlspecialchars($_POST['name']);
+    $description = htmlspecialchars($_POST['description']); 
+    // updateCategory($name, $description, $id);
+
+    updateCategory($name,$description, $id);
+    header('Location: /admin_categories');
 }
+
